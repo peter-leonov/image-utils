02b6ff75f37f2335c9732dc95529ecfc74cc35d7
Peter Leonov <gojpeg@gmail.com>
2011-10-16 01:56:23 +0400

    split open & mmap form the jpeg parsing logic

--- geometry.c
+++ geometry.c
@@ -9,26 +9,10 @@
 #include <sys/mman.h>
 
 int
-process (char const *srcfn)
+try_jpeg (u_char *data, size_t data_length, u_int *width, u_int *height)
 {
-	int fd;
-	u_char *data, type;
-	size_t data_length, i, block_length;
-	u_int height, width;
-	struct stat fs;
-	
-	fd = open(srcfn,  O_RDONLY);
-	if (fd == -1)
-	{
-		perror("ERROR: Can't open the file for reading");
-		return 2;
-	}
-	
-	fstat(fd, &fs);
-	
-	data_length = fs.st_size;
-	
-	data = mmap(0, data_length, PROT_READ, MAP_SHARED, fd, 0);
+	u_char type;
+	size_t i, block_length;
 	
 	i = 0;
 	
@@ -69,9 +53,8 @@ process (char const *srcfn)
 				return 4;
 			}
 			
-			height = (data[i+5] << 8) + data[i+6];
-			width = (data[i+7] << 8) + data[i+8];
-			printf("%dx%d\n", width, height);
+			*height = (data[i+5] << 8) + data[i+6];
+			*width = (data[i+7] << 8) + data[i+8];
 			return 0;
 		}
 		
@@ -83,6 +66,42 @@ process (char const *srcfn)
 		i += 2;
 		
 		block_length = (data[i] << 8) + data[i+1];
+	}
+	
+	return 6;
+}
+
+int
+process (char const *srcfn)
+{
+	int fd, rc;
+	u_char *data;
+	size_t data_length;
+	struct stat fs;
+	u_int height, width;
+	
+	fd = open(srcfn,  O_RDONLY);
+	if (fd == -1)
+	{
+		perror("ERROR: Can't open the file for reading");
+		return 2;
+	}
+	
+	fstat(fd, &fs);
+	
+	data_length = fs.st_size;
+	
+	data = mmap(0, data_length, PROT_READ, MAP_SHARED, fd, 0);
+	
+	rc = try_jpeg(data, data_length, &width, &height);
+	if (rc == 0)
+	{
+		printf("%dx%d\n", width, height);
+		return 0;
+	}
+	if (rc != -1)
+	{
+		return rc;
 	}
 	
 	return 6;
